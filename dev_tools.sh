a5a2fa7d2c30283d093044d96ea89e0359fefff5
Julian Nonino <noninojulian@outlook.com>
2018-04-13 17:39:55 -0300

    Updating Java version

--- dev_tools.sh
+++ dev_tools.sh
@@ -4,9 +4,9 @@
 ### Java
 ########################################################################################################################################
 echo "Installing Java JDK"
-wget --no-check-certificate --no-cookies --header "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/8u162-b12/0da788060d494f5095bf8624735fa2f1/jdk-8u162-linux-x64.tar.gz
-tar -zxvf jdk-8u162-linux-x64.tar.gz
-rm -rf jdk-8u162-linux-x64.tar.gz
+wget --no-check-certificate --no-cookies --header "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/10+46/76eac37278c24557a3c4199677f19b62/jdk-10_linux-x64_bin.tar.gz
+tar -zxvf jdk-10_linux-x64_bin.tar.gz
+rm -rf jdk-10_linux-x64_bin.tar.gz
 mv jdk1.8.0_162 /opt/java
 echo "export JAVA_HOME=/opt/java" >> /etc/profile.d/dev_tools.sh
 echo "export PATH=\$PATH:\$JAVA_HOME/bin:\$JAVA_HOME/lib" >> /etc/profile.d/dev_tools.sh
