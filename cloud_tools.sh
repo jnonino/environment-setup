e6fc3fab0727d1aa03d15b8e88644248e978c8e4
Julian <jnonino@users.noreply.github.com>
2018-05-19 20:03:12 -0300

    Updating Cloud tools versions

--- cloud_tools.sh
+++ cloud_tools.sh
@@ -20,20 +20,20 @@ echo "export PATH=\$PATH:\$TERRAFORM_HOME" >> /etc/profile.d/cloud_tools.sh
 ########################################################################################################################################
 # Install kubectl
 LATEST_VERSION=$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)
-KUBECTL_VERSION=v1.10.0
+KUBECTL_VERSION=v1.10.2
 echo "Installing kubectl"
 curl -LO https://storage.googleapis.com/kubernetes-release/release/${KUBECTL_VERSION}/bin/linux/amd64/kubectl
 chmod +x ./kubectl
 mkdir -p /opt/kubernetes
 mv kubectl /opt/kubernetes/
 # Install minikube
-MINIKUBE_VERSION=v0.25.2
+MINIKUBE_VERSION=v0.27.0
 echo "Installing minikube"
 curl -Lo minikube https://storage.googleapis.com/minikube/releases/${MINIKUBE_VERSION}/minikube-linux-amd64
 chmod +x minikube
 mv minikube /opt/kubernetes/
 # Install kompose
-KOMPOSE_VERSION=v1.11.0
+KOMPOSE_VERSION=v1.13.0
 echo "Installing kompose"
 curl -L https://github.com/kubernetes/kompose/releases/download/${KOMPOSE_VERSION}/kompose-linux-amd64 -o kompose
 chmod +x kompose
