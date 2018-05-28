c90a8cbb4868f6af0b2aeb28154b54d9d07d367f
Julian Nonino <noninojulian@outlook.com>
2018-05-28 17:12:24 -0300

    Making scripts executable

--- xmind.sh
+++ xmind.sh
@@ -1,21 +1,23 @@
 #!/bin/bash
 
-XMIND_VERSION=8
-XMIND_UPDATE=7
+#XMIND_VERSION=8
+#XMIND_UPDATE=7
 
-echo "Installing XMind"
+#echo "Installing XMind"
 
-wget http://dl2.xmind.net/xmind-downloads/xmind-$XMIND_VERSION-update$XMIND_UPDATE-linux.zip
-unzip xmind-$XMIND_VERSION-update$XMIND_UPDATE-linux.zip -d xmind
-mv xmind /opt/
-chmod a+w /opt/xmind/XMind_amd64/configuration
-mkdir -p /usr/share/fonts/truetype/xmind
-cp -R /opt/xmind/fonts/* /usr/share/fonts/truetype/xmind/
-fc-cache -f
+#wget http://dl2.xmind.net/xmind-downloads/xmind-$XMIND_VERSION-update$XMIND_UPDATE-linux.zip
+#unzip xmind-$XMIND_VERSION-update$XMIND_UPDATE-linux.zip -d xmind
+#mv xmind /opt/
+#chmod a+w /opt/xmind/XMind_amd64/configuration
+#mkdir -p /usr/share/fonts/truetype/xmind
+#cp -R /opt/xmind/fonts/* /usr/share/fonts/truetype/xmind/
+#fc-cache -f
+
+/opt/xmind/setup.sh
 
 echo "[Desktop Entry]" >> /usr/share/applications/xmind.desktop
 echo "Comment=Create and share mind maps." >> /usr/share/applications/xmind.desktop
-echo "Exec=/opt/xmind/$(DIR)/XMind %F" >> /usr/share/applications/xmind.desktop
+echo "Exec=/opt/xmind/XMind_amd64/XMind %F" >> /usr/share/applications/xmind.desktop
 echo "Name=XMind" >> /usr/share/applications/xmind.desktop
 echo "Terminal=false" >> /usr/share/applications/xmind.desktop
 echo "Type=Application" >> /usr/share/applications/xmind.desktop
