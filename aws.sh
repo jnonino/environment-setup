17774163ef544ad728bbd8a612456060d20c68b9
Julian Nonino <noninojulian@outlook.com>
2018-06-29 12:17:15 -0300

    Adding AWS Cli install script

---
+++ aws.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "Installing Python 3"
+apt-get -y update
+apt-get -y upgrade
+apt-get install -y python3 python3-pip
+
+echo "Installing AWS Cli"
+pip install awscli --upgrade --user
+
+echo "export PATH=~/.local/bin:$PATH" >> /etc/profile.d/aws.sh
+chmod +x /etc/profile.d/aws.sh
