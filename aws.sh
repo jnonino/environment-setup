fbdba691a4f949113339f561fc319f6e189f3d66
Julian Nonino <noninojulian@outlook.com>
2018-07-02 10:44:40 -0300

    Fixing error in AWS CLI installer

--- aws.sh
+++ aws.sh
@@ -8,5 +8,5 @@ apt-get install -y python3 python3-pip
 echo "Installing AWS Cli"
 pip install awscli --upgrade --user
 
-echo "export PATH=~/.local/bin:$PATH" >> /etc/profile.d/aws.sh
+echo "export PATH=~/.local/bin:\$PATH" >> /etc/profile.d/aws.sh
 chmod +x /etc/profile.d/aws.sh
