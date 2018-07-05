3aae574d30c96025aaf145bdfc0c9d347bc47d7a
Julian Nonino <noninojulian@gmail.com>
2018-07-05 10:44:22 -0300

    Fixing error in vagrant

--- vagrant.sh
+++ vagrant.sh
@@ -3,6 +3,6 @@
 VAGRANT_VERSION=2.1.2
 
 echo "Installing Vagrant"
-wget https://releases.hashicorp.com/vagrant/$VAGRANT_VERSION/vagrant_$VAGRANT_VERSION_x86_64.deb
-dpkg -i vagrant_$VAGRANT_VERSION_x86_64.deb
-rm -rf vagrant_$VAGRANT_VERSION_x86_64.deb
+wget https://releases.hashicorp.com/vagrant/${VAGRANT_VERSION}/vagrant_${VAGRANT_VERSION}_x86_64.deb
+dpkg -i vagrant_${VAGRANT_VERSION}_x86_64.deb
+rm -rf vagrant_${VAGRANT_VERSION}_x86_64.deb
