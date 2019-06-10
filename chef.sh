e143f3bdeb3097e11aeca6d87fd288feed810c32
Julian <jnonino@users.noreply.github.com>
2019-06-10 22:05:55 +0100

    Updating Chef version

--- chef.sh
+++ chef.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-CHEF_VERSION=3.2.30
+CHEF_VERSION=4.0.60
 UBUNTU_VERSION=18.04
 
 wget https://packages.chef.io/files/stable/chefdk/$CHEF_VERSION/ubuntu/$UBUNTU_VERSION/chefdk_$CHEF_VERSION-1_amd64.deb
