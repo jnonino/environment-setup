d9f4826c168a5649ce7e380b15bbfbbddc50cc73
Julian <jnonino@users.noreply.github.com>
2019-06-10 22:12:07 +0100

    Updating Java JDK version

--- java.sh
+++ java.sh
@@ -2,10 +2,10 @@
 
 echo "Installing Java JDK"
 
-wget --no-check-certificate --no-cookies --header "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/10.0.2+13/19aef61b38124481863b1413dce1855f/jdk-10.0.2_linux-x64_bin.tar.gz
-tar -zxvf jdk-10.0.2_linux-x64_bin.tar.gz
-rm -rf jdk-10.0.2_linux-x64_bin.tar.gz
-mv jdk-10.0.2 /opt/java
+wget --no-check-certificate --no-cookies --header "Cookie: oraclelicense=accept-securebackup-cookie" https://download.oracle.com/otn-pub/java/jdk/12.0.1+12/69cfe15208a647278a19ef0990eea691/jdk-12.0.1_linux-x64_bin.tar.gz
+tar -zxvf jdk-12.0.1_linux-x64_bin.tar.gz
+rm -rf jdk-12.0.1_linux-x64_bin.tar.gz
+mv jdk-12.0.1 /opt/java
 
 echo "export JAVA_HOME=/opt/java" >> /etc/profile.d/java.sh
 echo "export PATH=\$PATH:\$JAVA_HOME/bin:\$JAVA_HOME/lib" >> /etc/profile.d/java.sh
