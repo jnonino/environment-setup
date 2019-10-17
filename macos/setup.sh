36f6cbc6f9ae3893a3813bd0ac0eccbb65ab5758
Julian Nonino <jnonino@users.noreply.github.com>
2019-10-17 13:10:15 +0100

    Update setup.sh

--- macos/setup.sh
+++ macos/setup.sh
@@ -11,66 +11,9 @@ brew tap homebrew/cask
 brew tap cloudfoundry/tap
 
 # Upgrade all packages
-
 brew update
 brew upgrade
 Brew cask upgrade
-
-# Install Common Tools
-brew cask install google-chrome 
-brew cask install firefox
-brew cask install slack
-brew cask install zoomus
-brew cask install jetbrains-toolbox
-brew cask install visual-studio-code
-brew cask install sublime-text
-brew install curl
-brew install wget
-brew install coreutils
-
-# Install VCS Tools
-brew install git
-brew install git-crypt
-brew install git-lfs
-brew cask install github
-
-# Install Programming Languages and DevTools
-brew cask install java
-brew install ant
-brew install maven
-brew install gradle
-brew install python
-brew install go
-brew install node
-brew install ruby rbenv ruby-build
-rbenv install 2.6.3
-rbenv global 2.6.3
-echo 'if which rbenv > /dev/null; then eval "$(rbenv init -)"; fi' >> ~/.bash_profile
-source ~/.bash_profile
-gem install rails
-brew cask install fly
-brew cask install postman
-
-# Install Cloud and Virtualization Tools
-brew cask install docker
-brew install terraform
-brew install terragrunt
-brew install kubernetes-cli
-brew install kubernetes-helm
-rm /usr/local/bin/kubectl
-brew link --overwrite kubernetes-cli
-brew cask install minikube
-brew install kops
-brew install kubeless
-brew install awscli
-brew cask install google-cloud-sdk
-brew install cf-cli
-brew cask install vagrant
-brew cask install virtualbox
-
-# Databases
-brew cask install db-browser-for-sqlite
-brew cask install pgadmin4
 
 # Setup Zsh
 brew install zsh
@@ -78,19 +21,6 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/mas
 chsh -s $(which zsh)
 brew install zsh-syntax-highlighting
 echo 'source /usr/local/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh' >> ~/.zshrc
-brew install direnv
-echo 'eval "$(direnv hook zsh)"' >> ~/.zshrc
-brew install warrensbox/tap/tfswitch
-echo 'autoload -U add-zsh-hook' >> ~/.zshrc
-echo '' >> ~/.zshrc
-echo 'load-tfswitch() {' >> ~/.zshrc
-echo '  local tfswitchrc_path=".tfswitchrc"' >> ~/.zshrc
-echo '  if [ -f "$tfswitchrc_path" ]; then' >> ~/.zshrc
-echo '    tfswitch' >> ~/.zshrc
-echo '  fi' >> ~/.zshrc
-echo '}' >> ~/.zshrc
-echo 'add-zsh-hook chpwd load-tfswitch' >> ~/.zshrc
-echo 'load-tfswitch' >> ~/.zshrc
 brew install zsh-autosuggestions
 echo 'source /usr/local/share/zsh-autosuggestions/zsh-autosuggestions.zsh' >> ~/.zshrc
 brew install zsh-completions
@@ -116,5 +46,112 @@ echo 'setopt share_history          # Share command history data' >> ~/.zshrc
 echo "if [ $commands[kubectl] ]; then source <(kubectl completion zsh); fi" >> ~/.zshrc 
 #plugins=(colored-man-pages osx brew dotenv git vscode terraform python pip pipenv autopep8 pylint node npm aws docker docker-compose minikube kubectl kops helm)
 
+# Install Common Tools
+brew install coreutils
+brew install direnv
+echo 'eval "$(direnv hook zsh)"' >> ~/.zshrc
+
+# Browsers and web tools
+brew cask install google-chrome 
+brew cask install firefox
+brew install curl
+brew install wget
+brew cask install postman
+
+# Collaboration tools
+brew cask install slack
+brew cask install zoomus
+
+# IDEs
+brew cask install jetbrains-toolbox
+brew cask install visual-studio-code
+brew cask install sublime-text
+
+# Git
+brew install git
+brew install git-crypt
+brew install git-lfs
+brew cask install github
+
+# Java
+brew cask install java
+
+# Python
+brew install python
+
+# Ant
+brew install ant
+
+# Maven
+brew install maven
+
+# Gradle
+brew install gradle
+
+# Go
+brew install go
+
+# Node.js
+brew install node
+
+# Ruby
+brew install ruby rbenv ruby-build
+echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.zshenv
+echo 'source $HOME/.zshenv' >> ~/.zshrc
+echo 'eval "$(rbenv init - zsh)"' >> ~/.zshrc
+rbenv install 2.6.5
+rbenv global 2.6.5
+gem install rails
+
+# Terraform
+brew install terraform
+brew install terragrunt
+brew install warrensbox/tap/tfswitch
+echo 'autoload -U add-zsh-hook' >> ~/.zshrc
+echo '' >> ~/.zshrc
+echo 'load-tfswitch() {' >> ~/.zshrc
+echo '  local tfswitchrc_path=".tfswitchrc"' >> ~/.zshrc
+echo '  if [ -f "$tfswitchrc_path" ]; then' >> ~/.zshrc
+echo '    tfswitch' >> ~/.zshrc
+echo '  fi' >> ~/.zshrc
+echo '}' >> ~/.zshrc
+echo 'add-zsh-hook chpwd load-tfswitch' >> ~/.zshrc
+echo 'load-tfswitch' >> ~/.zshrc
+
+# Docker
+brew cask install docker
+
+# Kubernetes
+brew install kubernetes-cli
+brew install kubernetes-helm
+rm /usr/local/bin/kubectl
+brew link --overwrite kubernetes-cli
+brew cask install minikube
+brew install kops
+brew install kubeless
+echo "if [ $commands[kubectl] ]; then source <(kubectl completion zsh); fi" >> ~/.zshrc
+
+# Amazon Web Services
+brew install awscli
+
+# Google Cloud Platform
+brew cask install google-cloud-sdk
+
+# Cloudfoundry
+brew install cf-cli
+
+# Virtualbox
+brew cask install virtualbox
+
+# Vagrant
+brew cask install vagrant
+
+# Databases
+brew cask install db-browser-for-sqlite
+brew cask install pgadmin4
+
 # Clean up Brew
 brew cleanup
+
+# Apply changes to Zsh
+source ~/.zshrc
