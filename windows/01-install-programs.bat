41dd5c82ccf91aca6e0cbf22394345fca1919a31
Julian Nonino <noninojulian@outlook.com>
2020-03-29 18:35:18 +0100

    Adding more packages for windows

--- windows/01-install-programs.bat
+++ windows/01-install-programs.bat
@@ -1,10 +1,25 @@
-choco install terraform
-choco install nodejs
-choco install python
-choco install postman
-choco install notepadplusplus
-choco install git
-choco install awscli
-choco install adobereader
-choco install zoom
-choco install gr
+
+choco install -y nodejs
+choco install -y python
+choco install -y openjdk
+choco install -y gradle
+choco install -y maven
+
+
+choco install -y postman
+
+choco install -y jetbrainstoolbox
+choco install -y notepadplusplus
+
+choco install -y git
+choco install -y git-lfs.install
+
+choco install -y terraform
+choco install -y awscli
+
+choco install -y adobereader
+choco install -y firefox
+choco install -y zoom
+choco install -y virtualbox
+
+
