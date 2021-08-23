25234e26f8f62dc2ac8624be7514e46dbe28847a
Julian Nonino <jnonino@users.noreply.github.com>
2021-08-23 10:50:48 +0100

    Update terraform.sh

--- linux/terraform.sh
+++ linux/terraform.sh
@@ -1,15 +1,4 @@
 #!/bin/bash
 
-TERRAFORM_VERSION=0.12.1
-
-echo "Installing Terraform"
-
-wget https://releases.hashicorp.com/terraform/${TERRAFORM_VERSION}/terraform_${TERRAFORM_VERSION}_linux_amd64.zip
-unzip terraform_${TERRAFORM_VERSION}_linux_amd64.zip
-rm -rf terraform_${TERRAFORM_VERSION}_linux_amd64.zip
-mkdir -p /opt/terraform
-mv terraform /opt/terraform/
-
-echo "export TERRAFORM_HOME=/opt/terraform" >> /etc/profile.d/terraform.sh
-echo "export PATH=\$PATH:\$TERRAFORM_HOME" >> /etc/profile.d/terraform.sh
-chmod +x /etc/profile.d/terraform.sh
+echo "Installing TFSwitch"
+curl -L https://raw.githubusercontent.com/warrensbox/terraform-switcher/release/install.sh | bash
