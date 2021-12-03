bac259f1b4f2a166830fd86a1c3523b9ccac518d
Julian Nonino <jnonino@users.noreply.github.com>
2021-12-03 15:26:12 +0000

    Update 00-brew.sh

--- macos/00-brew.sh
+++ macos/00-brew.sh
@@ -4,12 +4,10 @@
 cd /usr/local
 mkdir homebrew && curl -L https://github.com/Homebrew/brew/tarball/master | tar xz --strip 1 -C homebrew
 
-brew tap homebrew/core
-brew tap homebrew/bundle
-brew tap homebrew/services
-brew tap homebrew/cask
+eval "$(homebrew/bin/brew shellenv)"
+brew update --force --quiet
+chmod -R go-w "$(brew --prefix)/share/zsh"
 
 # Upgrade all packages
 brew update
 brew upgrade
-Brew cask upgrade
