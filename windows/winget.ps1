33125cd9c6a12fac2871c0e6f3696a499e20652f
Julian Nonino <jnonino@users.noreply.github.com>
2022-09-06 15:24:36 +0100

    Add script to use winget

---
+++ windows/winget.ps1
@@ -0,0 +1,36 @@
+# How to Run:
+#   powershell -ExecutionPolicy Bypass -File .\winget.ps1
+
+$apps = @(
+  'Amazon.AWSCLI'
+  'Docker.DockerDesktop'
+  'Git.Git'
+  'GitHub.cli'
+  'GitHub.GitLFS'
+  'GitHub.GitHubDesktop'
+  'GoLang.Go.1.19'
+  'JetBrains.Toolbox'
+  'Kubernetes.minikube'
+  'Kubernetes.kubectl'
+  'Notepad++.Notepad++'
+  'OpenJS.NodeJS'
+  'Oracle.JDK.17'
+  'Postman.Postman'
+  'Python.Python.3.10'
+  'RedHat.Podman'
+)
+
+$apps | ForEach-Object {
+  $Vendor, $App = $_.Split(".")
+
+  winget list --id $_ | Out-Null
+  if ($LASTEXITCODE -eq 0) {
+    Write-Output "$App from $Vendor is already installed"
+  } else {
+    Write-Output "Installing $App from $Vendor"
+    winget install $_
+    if ($LASTEXITCODE -eq 0) {
+      Write-Output "$App from $Vendor installed successfully."
+    }
+  }
+}
