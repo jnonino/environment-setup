ce76ab532f9b429db1f898ffa0b51c6bef2bb4e9
Julian Nonino <jnonino@users.noreply.github.com>
2022-09-09 17:27:09 +0100

    Update winget.ps1

--- windows/winget.ps1
+++ windows/winget.ps1
@@ -6,23 +6,47 @@
 
 $apps = @(
   'Amazon.AWSCLI'
+  'Amazon.Kindle'
+  'Amazon.SendToKindle'
+  'Canonical.Ubuntu'
   'ChristianSchenk.MiKTeX'
+  'Dell.CommandUpdate'
   'Docker.DockerDesktop'
   'Git.Git'
   'GitHub.cli'
-  'GitHub.GitLFS'
   'GitHub.GitHubDesktop'
-  'GoLang.Go.1.19'
+  'GitHub.GitLFS'
+  'GoLang.Go'
+  'Google.AndroidStudio'
+  'Google.Chrome'
+  'Google.CloudSDK'
+  'Google.Drive'
   'JetBrains.Toolbox'
   'Kubernetes.minikube'
   'Kubernetes.kubectl'
+  'Microsoft.AzureCLI'
+  'Microsoft.Edge'
+  'Microsoft.EdgeWebView2Runtime'
+  'Microsoft.MouseandKeyboardCenter'
+  'Microsoft.MouseWithoutBorders'
+  'Microsoft.OneDrive'
+  'Microsoft.PowerShell'
+  'Microsoft.Teams'
+  'Microsoft.VisualStudioCode'
+  'Microsoft.WindowsTerminal'
+  'Mozilla.Firefox'
   'Notepad++.Notepad++'
   'OpenJS.NodeJS'
   'Oracle.JDK.17'
   'Postman.Postman'
-  'Python.Python.3.10'
+  'Python.Python.3'
   'RedHat.Podman'
+  'SlackTechnologies.Slack'
+  'Spotify.Spotify'
   'StrawberryPerl.StrawberryPerl'
+  'Valve.Steam'
+  'Zoom.Zoom'
+  'Zoom.Zoom.OutlookPlugin'
 )
 
 $apps | ForEach-Object {
@@ -33,7 +57,7 @@ $apps | ForEach-Object {
     Write-Output "$App from $Vendor is already installed"
   } else {
     Write-Output "Installing $App from $Vendor"
-    winget install $_
+    winget install --exact --id $_
     if ($LASTEXITCODE -eq 0) {
       Write-Output "$App from $Vendor installed successfully."
     }
